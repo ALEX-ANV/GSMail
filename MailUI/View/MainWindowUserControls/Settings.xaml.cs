9a330955b0afe9ee2e11c82783348613d9f13ce2
ANV <ANV@geosolution.by>
2017-03-29 15:50:26 +0300

    merge settings view

--- MailUI/View/MainWindowUserControls/Settings.xaml.cs
+++ MailUI/View/MainWindowUserControls/Settings.xaml.cs
@@ -6,19 +6,25 @@ namespace MailUI.View.MainWindowUserControls
     /// <summary>
     /// Interaction logic for Settings.xaml
     /// </summary>
-    public partial class Settings 
+    public partial class Settings
     {
+        private SettingsViewModel Model;
+
         public Settings()
         {
             InitializeComponent();
-            DataContext = new SettingsViewModel();
+            DataContext = Model = new SettingsViewModel();
         }
 
         private void Apply(object sender, RoutedEventArgs e)
         {
-            
+           
         }
 
 
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            this.OnRequestClose();
+        }
     }
 }
